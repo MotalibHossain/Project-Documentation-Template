c0044a12eeadf22c6860409f471f438ebbb56a31
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-05-28 17:08:35 +0600

    complete project setup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+Common Scripts (All Common codes calling)
+Project:    Theamforest Product documentation.
+Copyright:  #B4-developers.
+Developer:  #B4-developers.
+Author URI: 
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
